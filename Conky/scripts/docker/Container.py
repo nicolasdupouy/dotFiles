f6209b3fd7ad6b713f8ed309c90b628705dcbd65
Nicolas Dupouy <nicolasdupouy@gmail.com>
2019-06-17 14:15:18 +0200

    Display running container id and images

--- Conky/scripts/docker/Container.py
+++ Conky/scripts/docker/Container.py
@@ -1,23 +1,29 @@
 import subprocess
 
-from Conky.scripts.docker import Constants
+import Constants
 
 class Container:
 
     def __init__(self):
         container_ls = subprocess.check_output(Constants.DOCKER_CONTAINER_COMMAND)
-        self.command_result = container_ls.decode(Constants.UTF_8)
+        command_result = container_ls.decode(Constants.UTF_8)
+        self.valid_lines = self.filter_header_and_empty_lines(command_result)
 
     def count_containers(self):
-        valid_lines = self.filter_header_and_empty_lines()
-        line_number = len(valid_lines)
+        line_number = len(self.valid_lines)
         if line_number > 1:
             return "{} {}".format(Constants.RUNNING_CONTAINERS, line_number)
         else:
             return "{} {}".format(Constants.RUNNING_CONTAINER, line_number)
 
-    def filter_header_and_empty_lines(self):
-        return [line for line in self.command_result.split(Constants.NEW_LINE) if not self.is_header_or_empty_line(line)]
+    def display_container_names(self):
+        for index, line in enumerate(self.valid_lines):
+            elements = self.split_results(line)
+            (container_id, image) = self.extract_container_and_image_name(elements)
+            print("  => {}: {} / {}".format(index+1, container_id, image))
+
+    def filter_header_and_empty_lines(self, command_result):
+        return [line for line in command_result.split(Constants.NEW_LINE) if not self.is_header_or_empty_line(line)]
 
     def is_header_or_empty_line(self, line):
         return self.is_empty_line(line) or self.contains_header(line)
@@ -27,3 +33,9 @@ class Container:
 
     def contains_header(self, line):
         return Constants.CONTAINER_ID in line and Constants.IMAGE in line
+
+    def split_results(self, line):
+        return [elem for elem in line.split(' ') if len(elem) > 0]
+
+    def extract_container_and_image_name(self, elements):
+        return (elements[0], elements[1])
--- Conky/scripts/docker/docker-informations.py
+++ Conky/scripts/docker/docker-informations.py
@@ -1,41 +1,21 @@
 import subprocess
 
-from Conky.scripts.docker import Constants
-from Conky.scripts.docker.Container import Container
+import Constants
+from Container import Container
 
 
 def docker_machine():
-    #subprocess.run(["ls", "-l"])
     print("> docker-machine ls")
-    #subprocess.run(["docker-machine", "ls"])
     docker_machine_ls = subprocess.check_output(Constants.DOCKER_MACHINE_COMMAND)
-    #print("> docker-machine ls \n{}".format(docker_machine_ls))
     result = docker_machine_ls.decode(Constants.UTF_8)
     s = result.split(Constants.NEW_LINE)
-    #l1 = [element for element in result.split(NEW_LINE) if len(element) > 0]
-    #l2 = filter(lambda x: len(x) > 0, s)
     for elem in s:
         print(elem)
 
 def docker_container():
     container = Container()
     print(container.count_containers())
-    #s = command_result.split(NEW_LINE)
-    #print(command_result)
-
-
+    container.display_container_names()
 
 docker_machine()
 docker_container()
-
-
-
-
-#[print(elem) for elem in s]
-#print(type(docker_machine_ls.astype(str)))
-#print(docker_machine_ls)
-#print(docker_machine_ls.replace('"', '').replace("\n","").replace("\N","").replace("'", '').replace("[","").replace("]",""))
-
-
-#print("Hello Friend !")
-
