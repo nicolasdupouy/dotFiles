09fd0ff74f8bae40b13167be80fefff68334bbc3
Nicolas Dupouy <nicolasdupouy@gmail.com>
2019-06-26 17:31:45 +0200

    Refactor display script

--- Conky/scripts/src/docker/docker-informations.py
+++ Conky/scripts/src/docker/docker-informations.py
@@ -3,19 +3,30 @@ import subprocess
 from src.docker import Constants
 from src.docker.Container import Container
 
-def docker_machine():
-    print("> docker-machine ls")
-    docker_machine_ls = subprocess.check_output(Constants.DOCKER_MACHINE_COMMAND)
-    result = docker_machine_ls.decode(Constants.UTF_8)
-    s = result.split(Constants.NEW_LINE)
-    for elem in s:
-        print(elem)
+def display_machine():
+    display_command(Constants.DOCKER_MACHINE_COMMAND)
 
-def docker_container():
+def display_command(command):
+    print(get_command_to_display(command))
+
+def get_command_to_display(command):
+    result = [get_command_line_entered(command)] \
+           + get_command_output(command)
+    return "\n".join(result)
+
+def get_command_line_entered(command):
+    return '> ' + " ".join(command)
+
+def get_command_output(command):
+    docker_machine_ls = subprocess.check_output(command)
+    result = docker_machine_ls.decode(Constants.UTF_8)
+    return result.split(Constants.NEW_LINE)
+
+def display_container():
     container = Container()
     print(container.count_containers())
     print(container.get_container_names())
 
 if __name__ == '__main__':
-    docker_machine()
-    docker_container()
+    display_machine()
+    display_container()
