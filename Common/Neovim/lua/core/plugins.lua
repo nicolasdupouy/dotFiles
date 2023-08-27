f0c2935a4647c3b23d1a77c77e31b05622281f82
Nicolas Dupouy <nicolasdupouy@gmail.com>
2023-08-27 18:38:16 +0200

    fix: remove command-line completion plugin

--- Common/Neovim/lua/core/plugins.lua
+++ Common/Neovim/lua/core/plugins.lua
@@ -28,7 +28,6 @@ local plugins = {
     'hrsh7th/cmp-nvim-lsp',
     'hrsh7th/cmp-buffer',
     'hrsh7th/cmp-path',
-    'hrsh7th/cmp-cmdline',
     'hrsh7th/nvim-cmp',
     -- Snippets
     'L3MON4D3/LuaSnip',
