602b857fa0980bc329da31392958f963a572b1e5
Nicolas Dupouy <nicolasdupouy@gmail.com>
2023-11-20 00:24:36 +0100

    feat: add find profile directory method

--- MacOS/.profile.d/.profile.sh
+++ MacOS/.profile.d/.profile.sh
@@ -1,3 +1,31 @@
+function find_profile_directory() {
+  SOURCE=${BASH_SOURCE[0]}
+  while [ -L "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+    TARGET=$(readlink "$SOURCE")
+    if [[ $TARGET == /* ]]; then
+      #echo "SOURCE '$SOURCE' is an absolute symlink to '$TARGET'"
+      SOURCE=$TARGET
+    else
+      DIR=$( dirname "$SOURCE" )
+      #echo "SOURCE '$SOURCE' is a relative symlink to '$TARGET' (relative to '$DIR')"
+      SOURCE=$DIR/$TARGET # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+    fi
+  done
+  #echo "SOURCE is '$SOURCE'"
+  RDIR=$( dirname "$SOURCE" )
+  DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+  #if [ "$DIR" != "$RDIR" ]; then
+    #echo "DIR '$RDIR' resolves to '$DIR'"
+  #fi
+  #echo "DIR is '$DIR'"
+
+  local return_value=$DIR
+  echo "$return_value"
+}
+
+PROFILE_DIRECTORY=$(find_profile_directory)
+echo "Profile directory: $PROFILE_DIRECTORY"
+
 export BASH_SILENCE_DEPRECATION_WARNING=1
 
 # Bash completion
