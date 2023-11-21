c4cad1df9a7051dd272a0385bf6838b58b1e1cb0
Nicolas Dupouy <nicolasdupouy@gmail.com>
2023-11-21 21:43:57 +0100

    feat: extract Work profile configuration

--- MacOS/.profile.d/.profile.sh
+++ MacOS/.profile.d/.profile.sh
@@ -34,6 +34,7 @@ source ${PROFILE_DIRECTORY}/.profile-Ruby.sh
 source ${PROFILE_DIRECTORY}/.profile-Node.js.sh
 source ${PROFILE_DIRECTORY}/.profile-Infrastructure.sh
 source ${PROFILE_DIRECTORY}/.profile-Android.sh
+source ${PROFILE_DIRECTORY}/.profile-Work.sh
 
 # Aliases
 alias ls='lsd'
@@ -41,14 +42,6 @@ alias ll='lsd -lFtr'
 alias la='lsd -alFtr'
 alias nicolas_sha256sum='shasum -a 256 $1 --check sha256sums.txt 2>/dev/null | grep $1'
 alias wireshark_as_root='sudo /Applications/InstalledApps/Security/Wireshark.app/Contents/MacOS/Wireshark &'
-
-
-# [ FOR Linkcy ]
-export ENT_USERNAME=linkcy
-export ENT_TOKEN=1xGY32LUaBxV8RvUfA55
-
-alias nicolas_serve_redoc_partner='redoc-cli serve http://localhost:8080/v3/api-docs/partner  --ssr --watch -p 8091'
-alias nicolas_serve_redoc_internal='redoc-cli serve http://localhost:8080/v3/api-docs/internal  --ssr --watch -p 8092'
 
 alias SSH_connect_Linkcy_Paynovate_Proxy='ssh -i ~/.ssh/paynovateProxy.pem admin@13.39.147.239'
 alias SSH_connect_Linkcy_Lotus='ssh -i ~/.ssh/lotus.pem admin@15.188.54.100'
