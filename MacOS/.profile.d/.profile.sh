2496aca9db46849a497350e6bd5ad01acd21c0de
Nicolas Dupouy <nicolasdupouy@gmail.com>
2023-11-21 21:55:03 +0100

    feat: extract SSH profile configuration

--- MacOS/.profile.d/.profile.sh
+++ MacOS/.profile.d/.profile.sh
@@ -35,25 +35,12 @@ source ${PROFILE_DIRECTORY}/.profile-Node.js.sh
 source ${PROFILE_DIRECTORY}/.profile-Infrastructure.sh
 source ${PROFILE_DIRECTORY}/.profile-Android.sh
 source ${PROFILE_DIRECTORY}/.profile-Security.sh
+source ${PROFILE_DIRECTORY}/.profile-SSH.sh
 source ${PROFILE_DIRECTORY}/.profile-Work.sh
 
 # Aliases
 alias ls='lsd'
 alias ll='lsd -lFtr'
 alias la='lsd -alFtr'
-
-alias SSH_connect_Linkcy_Paynovate_Proxy='ssh -i ~/.ssh/paynovateProxy.pem admin@13.39.147.239'
-alias SSH_connect_Linkcy_Lotus='ssh -i ~/.ssh/lotus.pem admin@15.188.54.100'
-alias SSH_connect_Linkcy_GitLabRunner='ssh -i ~/.ssh/GitLabRunner.pem admin@35.180.66.45'
-alias SSH_connect_Linkcy_Website='ssh -i ~/.ssh/websiteAkrolab ec2-user@13.39.86.187'
-
-alias SSH_connect_Other_mood_lentilles_fr='ssh -i ~/.ssh/id_rsa_mood_lentilles ubuntu@52.47.156.249'
-
-#alias SSH_connect_Local_Neptune='ssh -i ~/.ssh/id_rsa_nicolas_Neptune nicolas@192.168.50.102'
-alias SSH_connect_Local_Chicago='ssh -i ~/.ssh/id_rsa_nicolas_Chicago nicolas@Chicago'
-#alias SSH_connect_Local_Jupiter='ssh -i ~/.ssh/id_rsa_nicolas_Jupiter nicolas@192.168.50.104'
-alias SSH_connect_Local_LosAngeles='ssh -i ~/.ssh/id_rsa_nicolas_LosAngeles nicolas@LosAngeles'
-alias SSH_connect_Local_NewYork='ssh -i ~/.ssh/id_rsa_nicolas_NewYork nicolas@NewYork'
-# Uranus TODO (à renommer ?)
 
 alias nicolas_weather='curl -sL wttr.in/Suresnes'
