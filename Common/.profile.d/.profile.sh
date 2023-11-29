ef4b9182d8d7b8cfd8f98e4e26e56caaf0f69b4a
Nicolas Dupouy <nicolasdupouy@gmail.com>
2023-11-29 13:06:44 +0100

    fix: Bash syntax error on MacOS

--- Common/.profile.d/.profile.sh
+++ Common/.profile.d/.profile.sh
@@ -3,7 +3,8 @@ SPECIFIC_PROFILE_DIRECTORY=$2
 SPECIFIC_PROFILE_NAME=$3
 DEBUG=$4
 
-if [[ $DEBUG == "YES" ]] then
+if [[ $DEBUG == "YES" ]]
+then
   echo "Common Profile directory: $1"
   echo "Specific Profile directory: $2"
   echo "Specific profile name: $3"
