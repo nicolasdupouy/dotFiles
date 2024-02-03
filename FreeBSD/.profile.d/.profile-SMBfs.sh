cacddfac275ee7d21f4e526449500bda5034562a
Nicolas Dupouy <nicolasdupouy@gmail.com>
2024-02-03 20:45:25 +0100

    feat: store FreeBSD Samba mounts (Synologu-DS216j)

---
+++ FreeBSD/.profile.d/.profile-SMBfs.sh
@@ -0,0 +1,9 @@
+# Synology-DS216j
+alias alias_mount_SMBfs_SynologyDS216j_Datas_Musics='sudo mount /mnt/NAS/Samba/Synology-DS216j/Datas_Musics/'
+alias alias_umount_SMBfs_SynologyDS216j_Datas_Musics='sudo umount /mnt/NAS/Samba/Synology-DS216j/Datas_Musics/'
+alias alias_mount_SMBfs_SynologyDS216j_Datas_Multimedia='sudo mount /mnt/NAS/Samba/Synology-DS216j/Datas_Multimedia/'
+alias alias_umount_SMBfs_SynologyDS216j_Datas_Multimedia='sudo umount /mnt/NAS/Samba/Synology-DS216j/Datas_Multimedia/'
+alias alias_mount_SMBfs_SynologyDS216j_Datas_Photos='sudo mount /mnt/NAS/Samba/Synology-DS216j/Datas_Photos/'
+alias alias_umount_SMBfs_SynologyDS216j_Datas_Photos='sudo umount /mnt/NAS/Samba/Synology-DS216j/Datas_Photos/'
+alias alias_mount_SMBfs_SynologyDS216j_Datas_Documents='sudo mount /mnt/NAS/Samba/Synology-DS216j/Datas_Documents/'
+alias alias_umount_SMBfs_SynologyDS216j_Datas_Documents='sudo umount /mnt/NAS/Samba/Synology-DS216j/Datas_Documents/'
