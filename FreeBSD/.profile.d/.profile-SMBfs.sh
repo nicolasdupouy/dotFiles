1e393d46b737b36cfe865e5d0b093e9a2d8fc86c
Nicolas Dupouy <nicolasdupouy@gmail.com>
2024-02-03 20:53:10 +0100

    feat: store FreeBSD Samba mounts (dns323)

--- FreeBSD/.profile.d/.profile-SMBfs.sh
+++ FreeBSD/.profile.d/.profile-SMBfs.sh
@@ -7,3 +7,13 @@ alias alias_mount_SMBfs_SynologyDS216j_Datas_Photos='sudo mount /mnt/NAS/Samba/S
 alias alias_umount_SMBfs_SynologyDS216j_Datas_Photos='sudo umount /mnt/NAS/Samba/Synology-DS216j/Datas_Photos/'
 alias alias_mount_SMBfs_SynologyDS216j_Datas_Documents='sudo mount /mnt/NAS/Samba/Synology-DS216j/Datas_Documents/'
 alias alias_umount_SMBfs_SynologyDS216j_Datas_Documents='sudo umount /mnt/NAS/Samba/Synology-DS216j/Datas_Documents/'
+
+# dns323
+alias alias_mount_SMBfs_dns323_Multimedia='sudo mount /mnt/NAS/Samba/dns323/Multimedia'
+alias alias_umount_SMBfs_dns323_Multimedia='sudo umount /mnt/NAS/Samba/dns323/Multimedia'
+
+alias alias_mount_SMBfs_dns323_Musique='sudo mount /mnt/NAS/Samba/dns323/Musique'
+alias alias_umount_SMBfs_dns323_Musique='sudo umount /mnt/NAS/Samba/dns323/Musique'
+
+#alias alias_mount_SMBfs_dns323_Docs='sudo mount /mnt/NAS/Samba/dns323/Docs'
+#alias alias_umount_SMBfs_dns323_Docs='sudo umount /mnt/NAS/Samba/dns323/Docs'
