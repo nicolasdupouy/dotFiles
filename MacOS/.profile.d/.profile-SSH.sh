000b9ddac8f655fe95fba319beb830f6ce0c906c
Nicolas Dupouy <nicolasdupouy@gmail.com>
2024-05-20 09:01:17 +0200

    refactor: reorganize MacOS SSH aliases and keys

--- MacOS/.profile.d/.profile-SSH.sh
+++ MacOS/.profile.d/.profile-SSH.sh
@@ -1,11 +1,19 @@
 # -- Aliases
-alias alias_SSH_connect_Linkcy_Paynovate_Proxy='ssh -i ~/.ssh/paynovateProxy.pem admin@13.39.147.239'
-alias alias_SSH_connect_Linkcy_Lotus='ssh -i ~/.ssh/lotus.pem admin@15.188.54.100'
-alias alias_SSH_connect_Linkcy_GitLabRunner='ssh -i ~/.ssh/GitLabRunner.pem admin@35.180.66.45'
-alias alias_SSH_connect_Linkcy_Website='ssh -i ~/.ssh/websiteAkrolab ec2-user@13.39.86.187'
+# Local network
+alias alias_SSH_connect_Local_Chicago='ssh -i ~/.ssh/Local/id_rsa_nicolas_Chicago nicolas@Chicago'
+alias alias_SSH_connect_Local_NewYork='ssh -i ~/.ssh/Local/id_rsa_nicolas_NewYork nicolas@NewYork'
+alias alias_SSH_connect_Local_LosAngeles='ssh -i ~/.ssh/Local/id_rsa_nicolas_LosAngeles nicolas@LosAngeles'
 
-alias alias_SSH_connect_Other_mood_lentilles_fr='ssh -i ~/.ssh/id_rsa_mood_lentilles ubuntu@52.47.156.249'
+# LinkCy
+alias alias_SSH_connect_Linkcy_Paynovate_Proxy='ssh -i ~/.ssh/LinkCy/paynovateProxy.pem admin@13.39.147.239'
+alias alias_SSH_connect_Linkcy_Lotus='ssh -i ~/.ssh/LinkCy/lotus.pem admin@15.188.54.100'
+alias alias_SSH_connect_Linkcy_GitLabRunner='ssh -i ~/.ssh/LinkCy/GitLabRunner.pem admin@35.180.66.45'
+alias alias_SSH_connect_Linkcy_Website='ssh -i ~/.ssh/LinkCy/websiteAkrolab ec2-user@13.39.86.187'
+alias alias_SSH_connect_Linkcy_OnboardingDocuments='ssh -i ~/.ssh/LinkCy/OnboardingDocuments.pem admin@13.38.129.225'
 
-alias alias_SSH_connect_Local_Chicago='ssh -i ~/.ssh/id_rsa_nicolas_Chicago nicolas@Chicago'
-alias alias_SSH_connect_Local_NewYork='ssh -i ~/.ssh/id_rsa_nicolas_NewYork nicolas@NewYork'
-alias alias_SSH_connect_Local_LosAngeles='ssh -i ~/.ssh/id_rsa_nicolas_LosAngeles nicolas@LosAngeles'
+# Other
+## Hanane
+alias alias_SSH_connect_Other_Hanane_Hostinger='ssh -i ~/.ssh/Other/Hanane/website-Hanane-Hostinger_id_rsa -p 65002 u157694837@89.117.169.125'
+## Mathieu
+alias alias_SSH_connect_Other_Mathieu_mood_lentilles_fr='ssh -i ~/.ssh/Other/Mathieu_bottausci/id_rsa_mood_lentilles ubuntu@52.47.156.249'
+
