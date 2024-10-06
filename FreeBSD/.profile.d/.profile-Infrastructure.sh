c88edf9aa45d97e334d4fa360ef03da07ec1a37f
Nicolas Dupouy <nicolasdupouy@gmail.com>
2024-10-06 19:34:53 +0200

    feat: [FreeBSD] Kubernetes CLI setup (kubecolor)

--- FreeBSD/.profile.d/.profile-Infrastructure.sh
+++ FreeBSD/.profile.d/.profile-Infrastructure.sh
@@ -1,7 +1,13 @@
 # -- Tools
 # [Kubernetes]
+## [`k`] (Alias)
 alias k='kubectl'
 
 # Bash completion for "k" as "kubectl"
 source <(kubectl completion bash)
 complete -o default -F __start_kubectl k
+
+## [`kubecolor`]
+alias kubectl="kubecolor"
+# Bash completion for "kubecolor" as "kubectl"
+complete -o default -F __start_kubectl kubecolor
